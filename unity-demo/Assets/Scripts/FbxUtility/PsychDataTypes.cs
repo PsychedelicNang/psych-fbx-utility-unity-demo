4017aece9478e86d9ad22d430e85501d51d8a624
Brandon Fritz <brandonjfritz@gmail.com>
2023-02-28 20:11:14 -0600

    Added C# side classes to Unity. Fixed issue with loading materials (Textures were going out of bounds)

---
+++ unity-demo/Assets/Scripts/FbxUtility/PsychDataTypes.cs
@@ -0,0 +1,257 @@
+#define USE_MATERIALS
+#define USE_MESHES
+
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using UnityEngine;
+
+namespace Psych
+{
+    namespace CMath
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Vector2
+        {
+            public float x;
+            public float y;
+
+            public override string ToString()
+            {
+                return $"{x}, {y}";
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Vector3
+        {
+            public float x;
+            public float y;
+            public float z;
+
+            public override string ToString()
+            {
+                return $"{x}, {y}, {z}";
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Vector4
+        {
+            public float x;
+            public float y;
+            public float z;
+            public float w;
+
+            public override string ToString()
+            {
+                return $"{x}, {y}, {z}, {w}";
+            }
+        }
+    }
+
+    namespace CMaterialInfo
+    {
+        public enum PropertyType
+        {
+            PROPERTYTYPE_EMISSIVE = 0,
+            PROPERTYTYPE_AMBIENT,
+            PROPERTYTYPE_DIFFUSE,
+            PROPERTYTYPE_NORMAL,
+            PROPERTYTYPE_BUMP,
+            PROPERTYTYPE_TRANSPARENCY,
+            PROPERTYTYPE_DISPLACEMENT,
+            PROPERTYTYPE_VECTOR_DISPLACEMENT,
+            PROPERTYTYPE_SPECULAR,
+            PROPERTYTYPE_SHININESS,
+            PROPERTYTYPE_REFLECTION,
+            PROPERTYTYPE_COUNT
+        };
+
+        public enum MaterialType
+        {
+            MATERIALTYPE_PHONG = 0,
+            MATERIALTYPE_LAMBERT
+        };
+    }
+
+    enum CRESULT
+    {
+        CRESULT_SUCCESS = 0,
+        CRESULT_INCORRECT_FILE_PATH,
+        CRESULT_NO_OBJECTS_IN_SCENE,
+        CRESULT_NODE_WAS_NOT_GEOMETRY_TYPE,
+        CRESULT_ROOT_NODE_NOT_FOUND
+    };
+
+    #region Class Definitions
+
+    #if USE_MATERIALS
+    public struct CPPMaterial
+    {
+        public struct CPPPropertyData
+        {
+            public CMaterialInfo.PropertyType m_propertyType;
+            public IntPtr m_textureRelativeFileName;
+            public string TextureRelativeFileName => Marshal.PtrToStringAnsi(m_textureRelativeFileName);
+
+            public IntPtr m_textureAbsoluteFilePath;
+            public string TextureAbsoluteFilePath => Marshal.PtrToStringAnsi(m_textureAbsoluteFilePath);
+
+            public CMath.Vector4 m_dataColorValues;
+        };
+
+        public CMaterialInfo.MaterialType m_materialType;
+        public IntPtr m_materialProperties;
+
+        public CPPPropertyData[] MaterialProperties =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CPPPropertyData>(m_materialProperties, (int)CMaterialInfo.PropertyType.PROPERTYTYPE_COUNT);
+
+        public Texture2D[] Textures
+        {
+            get
+            {
+                Texture2D[] result = new Texture2D[m_textureCount];
+
+                int currentTextureIndex = 0;
+                var props = MaterialProperties;
+                for (int i = 0; i < props.Length; i++)
+                {
+                    if (props[i].TextureRelativeFileName != null ||
+                        props[i].TextureAbsoluteFilePath != null)
+                    {
+                        result[currentTextureIndex] = MarshalHelpers.LoadPNG(props[i].TextureAbsoluteFilePath);
+                        ++currentTextureIndex;
+                    }
+                }
+        
+                return result;
+            }
+        }
+
+        public uint m_textureCount;
+    }
+    #endif
+
+    #if USE_MESHES
+    public struct CPPMesh
+    {
+        public IntPtr m_allVerticesPositions;
+        public CMath.Vector3[] VertexPositions =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CMath.Vector3>(m_allVerticesPositions, (int)m_vertexCount);
+
+        public IntPtr m_normals;
+
+        public CMath.Vector3[] Normals =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CMath.Vector3>(m_normals, (int)m_vertexCount);
+
+        public IntPtr m_uvs;
+
+        public CMath.Vector2[] UVs =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CMath.Vector2>(m_uvs, (int)m_vertexCount);
+
+        public IntPtr m_indices;
+
+        public uint[] Indices =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<uint>(m_indices, (int)m_indexCount);
+
+        public int[] ConvertedIndices =>
+            Array.ConvertAll(Indices, val => checked((int)val));
+
+        public uint m_vertexCount;
+        public uint m_indexCount;
+    }
+    #endif
+
+    public struct CPPObject
+    {
+        public int m_parentArrayIndexID;
+        public IntPtr m_childrenArrayIndexIDs;
+
+    #if USE_MESHES
+        public IntPtr m_mesh;
+
+        public CPPMesh CPPMesh => Marshal.PtrToStructure<CPPMesh>(m_mesh);
+    #endif
+    #if USE_MATERIALS
+        public IntPtr m_materials;
+
+        public CPPMaterial[] CPPMaterials => 
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CPPMaterial>(m_materials, (int)m_numberOfMaterials);
+    #endif
+
+        public uint m_numberOfChildren;
+        public uint m_numberOfMaterials;
+
+        public IntPtr m_name;
+
+        public string Name =>
+            Marshal.PtrToStringAnsi(m_name);
+
+        public uint m_arrayIndexID;
+    }
+
+    public struct CPPImportedFBXScene
+    {
+        public IntPtr m_objects;
+        public uint m_numberOfObjects;
+
+        public CPPObject[] CPPObjects =>
+            MarshalHelpers.MarshalUnmananagedArray2Struct<CPPObject>(m_objects, (int) m_numberOfObjects);
+    }
+
+    public class MarshalHelpers
+    {
+        public static void MarshalUnmananagedArray2Struct<T>(IntPtr unmanagedArray, int length, out T[] mangagedArray)
+        {
+            var size = Marshal.SizeOf(typeof(T));
+            mangagedArray = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                IntPtr ins = new IntPtr(unmanagedArray.ToInt64() + i * size);
+                mangagedArray[i] = Marshal.PtrToStructure<T>(ins);
+            }
+        }
+
+        public static T[] MarshalUnmananagedArray2Struct<T>(IntPtr unmanagedArray, int length)
+        {
+            var size = Marshal.SizeOf(typeof(T));
+            T[] mangagedArray = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                IntPtr ins = new IntPtr(unmanagedArray.ToInt64() + i * size);
+                mangagedArray[i] = Marshal.PtrToStructure<T>(ins);
+            }
+
+            return mangagedArray;
+        }
+        
+        public static Texture2D LoadPNG(string _filePath)
+        {
+            Texture2D tex = null;
+            byte[] fileData;
+
+            if (File.Exists(_filePath))
+            {
+                fileData = File.ReadAllBytes(_filePath);
+                tex = new Texture2D(1, 1);
+                tex.LoadImage(fileData); // LoadImage() auto resizes the texture dimensions.
+            }
+
+            else
+            {
+                return null;
+            }
+
+            return tex;
+        }
+    }
+
+    public struct CPPFBXHandler
+    {
+        public IntPtr m_fbxScene;
+
+        public CPPImportedFBXScene CPPFBXScene => Marshal.PtrToStructure<CPPImportedFBXScene>(m_fbxScene);
+    }
+    #endregion
+}
---
+++ unity-demo/Assets/Scripts/FbxUtility/PsychFbxUtility.cs
@@ -0,0 +1,269 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Psych
+{
+    public static class PsychFbxUtilityDll
+    {
+        [DllImport("psychfbxutility")]
+        public static extern IntPtr CreateFBXHandler();
+
+        [DllImport("psychfbxutility")]
+        public static extern void DestroyFBXHandler(IntPtr fbxHandlerObject);
+
+        [DllImport("psychfbxutility", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int LoadFBXFile(IntPtr fbxHandlerObject, string fbxFilePath);
+    }
+    
+    public class PsychFbxUtility : MonoBehaviour
+    {
+        #region Member Variables
+        private static CPPFBXHandler m_managedFBXHandler;
+        static IntPtr unmanagedFBXHandler;
+
+        MeshFilter          m_unityMeshFilter;
+        Mesh                m_unityMesh;
+        MeshRenderer        m_unityMeshRenderer;
+        Transform           m_unityObjectTransform;
+       
+        public List<GameObject>    m_unityGameObjects;
+        #endregion
+
+        #region Class Methods
+        void Start () {
+            TimeSpan m_durationFileLoad;
+            TimeSpan m_durationFileParsed;
+
+            m_unityMeshFilter = gameObject.AddComponent<MeshFilter>();
+            m_unityMesh = m_unityMeshFilter.mesh;
+            m_unityMeshRenderer = gameObject.AddComponent<MeshRenderer>();
+            m_unityObjectTransform = gameObject.GetComponent<Transform>();
+
+            // C++ handler which is unmanaged memory (we need to delete by calling DLLDestroyFBXHandler())
+            unmanagedFBXHandler = PsychFbxUtilityDll.CreateFBXHandler();
+
+            DateTime m_timeBeforeFileLoad = DateTime.Now;
+
+            var fbxPath = "../Submodules/psych-fbx-utility/psych-fbx-utility-vs/resources/SM_PistolArnold.fbx";
+
+            CRESULT result = (CRESULT)PsychFbxUtilityDll.LoadFBXFile(unmanagedFBXHandler, fbxPath);
+		    DateTime m_timeAfterFileLoad = DateTime.Now;
+		    print (result);
+            m_durationFileLoad = m_timeAfterFileLoad - m_timeBeforeFileLoad;
+
+            switch (result)
+            {
+                case CRESULT.CRESULT_SUCCESS:
+                    {
+                        DateTime m_timeBeforeFileParsed = DateTime.Now;
+                        CSParseFBXHandler();
+                        DateTime m_timeAfterFileParsed = DateTime.Now;
+
+                        m_durationFileParsed = m_timeAfterFileParsed - m_timeBeforeFileParsed;
+
+                        if (m_durationFileLoad.Seconds > 0)
+                            print("The FBX File loaded in: " + m_durationFileLoad.Seconds + "." + m_durationFileLoad.Milliseconds + " s");
+                        else
+                            print("The FBX File loaded in: " + m_durationFileLoad.Milliseconds + " ms");
+
+                        if (m_durationFileParsed.Seconds > 0)
+                            print("The FBX File was parsed in: " + m_durationFileParsed.Seconds + "." + m_durationFileParsed.Milliseconds + " s");
+                        else
+                            print("The FBX File was parsed in: " + m_durationFileParsed.Milliseconds + " ms");
+                    }
+                    break;
+                case CRESULT.CRESULT_INCORRECT_FILE_PATH:
+                    print("Incorrect File Path.");
+                    break;
+                case CRESULT.CRESULT_NO_OBJECTS_IN_SCENE:
+                    print("There were no objects in the given FBX scene.");
+                    break;
+                case CRESULT.CRESULT_NODE_WAS_NOT_GEOMETRY_TYPE:
+                    print("Please, make sure the FBX scene only contains geometry nodes.");
+                    break;
+                case CRESULT.CRESULT_ROOT_NODE_NOT_FOUND:
+                    print("The root node of the FBX file was not found.");
+                    break;
+                default:
+                    break;
+            }
+
+            m_unityObjectTransform.localScale = new Vector3(10.0f, 10.0f, 10.0f);
+            m_unityObjectTransform.Translate(new Vector3(0.0f, 3.0f, 0.0f));
+        }
+
+        void CSParseFBXHandler()
+        {
+            m_managedFBXHandler = (CPPFBXHandler)Marshal.PtrToStructure(unmanagedFBXHandler, typeof(CPPFBXHandler));
+
+            m_unityGameObjects = new List<GameObject>((int)m_managedFBXHandler.CPPFBXScene.m_numberOfObjects);
+
+            CPPObject[] sceneObjects = m_managedFBXHandler.CPPFBXScene.CPPObjects;
+
+            for (uint currObjectIndex = 0;
+                 currObjectIndex < (int)m_managedFBXHandler.CPPFBXScene.m_numberOfObjects;
+                 currObjectIndex++)
+            {
+                GameObject unityGameObject = new GameObject();
+                m_unityGameObjects.Add(unityGameObject);
+
+                MeshFilter currMeshFilter = unityGameObject.AddComponent<MeshFilter>();
+                MeshRenderer currMeshRenderer = unityGameObject.AddComponent<MeshRenderer>();
+
+                unityGameObject.name = sceneObjects[currObjectIndex].Name;
+
+                print("Object: " + sceneObjects[currObjectIndex].Name + "\tMaterial Count: " + sceneObjects[currObjectIndex].m_numberOfMaterials + "\tNumber of Children: " + sceneObjects[currObjectIndex].m_numberOfChildren);
+
+                // Mesh info
+                if (sceneObjects[currObjectIndex].CPPMesh.m_vertexCount > 0)
+                {
+                    CMath.Vector3[] vertices = sceneObjects[currObjectIndex].CPPMesh.VertexPositions;
+                    CMath.Vector3[] normals = sceneObjects[currObjectIndex].CPPMesh.Normals;
+                    CMath.Vector2[] uvs = sceneObjects[currObjectIndex].CPPMesh.UVs;
+
+                    Vector3[] unityVerts = new Vector3[sceneObjects[currObjectIndex].CPPMesh.m_vertexCount];
+                    Vector3[] unityNormals = new Vector3[sceneObjects[currObjectIndex].CPPMesh.m_vertexCount];
+                    Vector2[] unityUVs = new Vector2[sceneObjects[currObjectIndex].CPPMesh.m_vertexCount];
+                
+                    for (uint currVertex = 0; currVertex < sceneObjects[currObjectIndex].CPPMesh.m_vertexCount; currVertex++)
+                    {
+                        unityVerts[currVertex].x = vertices[currVertex].x;
+                        unityVerts[currVertex].y = vertices[currVertex].y;
+                        unityVerts[currVertex].z = vertices[currVertex].z;
+
+                        unityNormals[currVertex].x = normals[currVertex].x;
+                        unityNormals[currVertex].y = normals[currVertex].y;
+                        unityNormals[currVertex].z = normals[currVertex].z;
+
+                        unityUVs[currVertex].x = uvs[currVertex].x;
+                        unityUVs[currVertex].y = uvs[currVertex].y;
+                    }
+
+                    var mesh = currMeshFilter.mesh;
+                    mesh.vertices = unityVerts;
+                    mesh.normals = unityNormals;
+                    mesh.uv = unityUVs;
+                    mesh.triangles = sceneObjects[currObjectIndex].CPPMesh.ConvertedIndices;
+                    
+                }
+                
+                // Material info
+                if (sceneObjects[currObjectIndex].m_numberOfMaterials > 0)
+                {
+                    currMeshRenderer.materials = new Material[sceneObjects[currObjectIndex].m_numberOfMaterials];
+
+                    for (int currMaterialIndex = 0; currMaterialIndex < sceneObjects[currObjectIndex].m_numberOfMaterials; currMaterialIndex++)
+                    {
+                        CPPMaterial[] materials = sceneObjects[currObjectIndex].CPPMaterials;
+                        
+                        currMeshRenderer.materials[currMaterialIndex].shader = Shader.Find("Standard");
+
+                        uint currentTextureIndex = 0;
+
+                        Texture2D[] textures = null;
+                        if (materials[currMaterialIndex].m_textureCount > 0)
+                        {
+                            textures = materials[currMaterialIndex].Textures;
+                        }
+                        
+                        for (int propertyIndex = 0; propertyIndex < (int)CMaterialInfo.PropertyType.PROPERTYTYPE_COUNT; propertyIndex++)
+                        {
+                            CPPMaterial.CPPPropertyData[] propertyData = materials[currMaterialIndex].MaterialProperties;
+                            if (propertyData[propertyIndex].TextureRelativeFileName != null ||
+                                propertyData[propertyIndex].TextureAbsoluteFilePath != null)
+                            {
+                                Color color;
+                                color.r = propertyData[propertyIndex].m_dataColorValues.x;
+                                color.g = propertyData[propertyIndex].m_dataColorValues.y;
+                                color.b = propertyData[propertyIndex].m_dataColorValues.z;
+                                color.a = propertyData[propertyIndex].m_dataColorValues.w;
+                                
+                                switch (propertyData[propertyIndex].m_propertyType)
+                                {
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_EMISSIVE:
+                                        {
+                                            
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_EMISSION");
+                                            currMeshRenderer.materials[currMaterialIndex].SetTexture("_EmissionMap", textures[currentTextureIndex]);
+                                            currMeshRenderer.materials[currMaterialIndex].SetColor("_EmissionColor", color);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_AMBIENT:
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_DIFFUSE:
+                                        {
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_MainTex");
+                                            currMeshRenderer.materials[currMaterialIndex].SetTexture("_MainTex", textures[currentTextureIndex]);
+                                            currMeshRenderer.materials[currMaterialIndex].SetColor("_MainTex", color);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_NORMAL:
+                                        {
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_BumpMap");
+                                            currMeshRenderer.materials[currMaterialIndex].SetTexture("_BumpMap", textures[currentTextureIndex]);
+                                            currMeshRenderer.materials[currMaterialIndex].SetFloat("_BumpScale", color.a);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_BUMP:
+                                        {
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_BumpMap");
+                                            currMeshRenderer.materials[currMaterialIndex].SetTexture("_BumpMap", textures[currentTextureIndex]);
+                                            currMeshRenderer.materials[currMaterialIndex].SetFloat("_BumpScale", color.a);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_TRANSPARENCY:
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_DISPLACEMENT:
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_VECTOR_DISPLACEMENT:
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_SPECULAR:
+                                        {
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_METALLICGLOSSMAP");
+                                            currMeshRenderer.materials[currMaterialIndex].SetTexture("_MetallicGlossMap", textures[currentTextureIndex]);
+                                            currMeshRenderer.materials[currMaterialIndex].SetFloat("Metallic", color.a);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_SHININESS:
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_REFLECTION:
+                                        {
+                                            currMeshRenderer.materials[currMaterialIndex].EnableKeyword("_Glossiness");
+                                            currMeshRenderer.materials[currMaterialIndex].SetFloat("Smoothness", color.a);
+                                        }
+                                        break;
+                                    case CMaterialInfo.PropertyType.PROPERTYTYPE_COUNT:
+                                        break;
+                                    default:
+                                        break;
+                                }
+
+                                ++currentTextureIndex;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            for (uint currObjectIndex = 0; currObjectIndex < m_managedFBXHandler.CPPFBXScene.m_numberOfObjects; currObjectIndex++)
+            {
+                m_unityGameObjects[(int)currObjectIndex].transform.parent = m_unityGameObjects[(int)sceneObjects[currObjectIndex].m_parentArrayIndexID].transform;
+            }
+
+            m_unityGameObjects[0].transform.parent = m_unityObjectTransform;
+        }
+        
+        private void OnDestroy()
+        {
+            // Delete C++ handler which is unmanaged memory.
+            PsychFbxUtilityDll.DestroyFBXHandler(unmanagedFBXHandler);
+
+            unmanagedFBXHandler = IntPtr.Zero;
+        }
+        #endregion
+    }
+}
